85dc9141ff29dae8ccfb02fc4771e99ca1533b24
JonFromNYC <82.Garcia@gmail.com>
2020-08-01 12:41:15 -0400

    Updates

---
+++ precache-manifest.955f8c17230148a4b2787db0129e5a22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e2a99b1da4978624214355e309d30d",
+    "url": "/react-redux-webpack/index.html"
+  },
+  {
+    "revision": "a1a33a46bcc2121ec9c9",
+    "url": "/react-redux-webpack/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d22c8a207100f1a297e9",
+    "url": "/react-redux-webpack/static/css/main.54224bf0.chunk.css"
+  },
+  {
+    "revision": "a1a33a46bcc2121ec9c9",
+    "url": "/react-redux-webpack/static/js/2.49ba0e03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-webpack/static/js/2.49ba0e03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d22c8a207100f1a297e9",
+    "url": "/react-redux-webpack/static/js/main.b8e95e00.chunk.js"
+  },
+  {
+    "revision": "ccc5c07f037f2fca82a9",
+    "url": "/react-redux-webpack/static/js/runtime-main.c5c22e85.js"
+  }
+]);
