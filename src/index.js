f7c998762edf9b748c4017bee31ef35a95c0e828
JonFromNYC <82.Garcia@gmail.com>
2020-08-01 13:57:30 -0400

    removed redux console logging

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider, connect } from 'react-redux';
+import {Provider } from 'react-redux';
 import {createStore, applyMiddleware, combineReducers } from 'redux';
-import './index.css';
 import * as serviceWorker from './serviceWorker';
 import { searchRobots, requestRobots } from './reducers';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import 'tachyons';
+import './index.css';
 import App from './containers/App.component';
 
 const logger = createLogger();
 const rootReducer = combineReducers({ searchRobots, requestRobots })
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware,logger));
+// const store = createStore(rootReducer, applyMiddleware(thunkMiddleware,logger));
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(
   <React.StrictMode>
